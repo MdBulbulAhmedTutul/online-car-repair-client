5614840af66d6560da4cfc56c8001fc6ac2cbebe
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-11-20 23:44:18 -0800

    design the navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,14 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from '../../assets/logo.svg';
+
 const Navbar = () => {
+    const navItem = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li className="ml-2"><NavLink to="/about">About</NavLink></li>
+        <li className="ml-2"><NavLink to="/services">Services</NavLink></li>
+        <li className="ml-2"><NavLink to="/blog">Blog</NavLink></li>
+        <li className="ml-2"><NavLink to="/contact">Contatct</NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -7,36 +17,21 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52 ">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navItem}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <Link to="/">
+                    <img className="w-[70px]" src={logo} alt="" />
+                </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navItem}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <Link>
+                    <button className="bg-[#ff3438] text-white px-6 py-2 rounded-lg font-bold">Login</button></Link>
             </div>
         </div>
     );
