7957c12b26962579d39e4d4db2dbf2ce86350aa3
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-11-20 23:47:05 -0800

    added the error page

---
+++ src/components/Erropage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Sorry</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,13 @@ import {
 } from "react-router-dom";
 import Root from './components/Root/Root';
 import Home from './Pages/Home/Home';
+import ErrorPage from './components/Erropage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
