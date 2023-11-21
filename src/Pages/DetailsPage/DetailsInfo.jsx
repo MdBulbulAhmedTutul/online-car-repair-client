2e459525118ea246f3385ff35432f0eff47000fb
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-11-21 07:50:36 -0800

    design the details info and rightSide

--- src/Pages/DetailsPage/DetailsInfo.jsx
+++ src/Pages/DetailsPage/DetailsInfo.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 const DetailsInfo = () => {
-    // http://localhost:5000/service/655ca4b0166c44afa3efe5e6
+
+    const [details, setDetails] = useState([]);
+    const singleData = useParams();
+    console.log(singleData)
+    useEffect(() => {
+        fetch(`http://localhost:5000/service/${singleData.id}`)
+            .then(res => res.json())
+            .then(data => setDetails(data))
+    }, [singleData])
+    console.log(details);
+    const { img, title, description} = details;
     return (
-        <div>
-            <h2>details info</h2>
+        <div className="px-4">
+            <img className="w-full rounded-lg h-[380px]" src={img} alt="" />
+            <h2 className="text-3xl font-semibold my-4">{title}</h2>
+            <p>{description}</p>
         </div>
     );
 };
--- src/Pages/DetailsPage/RightSideNav.jsx
+++ src/Pages/DetailsPage/RightSideNav.jsx
@@ -1,8 +1,51 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaRegFileWord } from "react-icons/fa";
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>right side</h2>
+            <div className="bg-[#F3F3F3] px-6 py-6 rounded-lg">
+                <h2 className="text-2xl font-semibold mb-2">Services</h2>
+                <button className="hover:bg-[#ff3438] mb-2 hover:text-white bg-white text-black px-4 py-2 flex items-center justify-evenly rounded-md w-full">
+                    <span>Full Car Repair</span>
+                    <FaLongArrowAltRight className="text-2xl mb-2"></FaLongArrowAltRight>
+                </button>
+                <button
+                    className="hover:bg-[#ff3438] mb-2 hover:text-white bg-white text-black px-4 py-2 flex items-center justify-evenly rounded-md w-full"><span>Engine Repair</span>
+                    <FaLongArrowAltRight className="text-2xl mb-2"></FaLongArrowAltRight>
+                </button>
+                <button
+                    className="hover:bg-[#ff3438] mb-2 hover:text-white bg-white text-black px-4 py-2 flex items-center justify-evenly rounded-md w-full"><span>Automatic Services</span>
+                    <FaLongArrowAltRight className="text-2xl mb-2"></FaLongArrowAltRight>
+                </button>
+                <button
+                    className="hover:bg-[#ff3438] mb-2 hover:text-white bg-white text-black px-4 py-2 flex items-center justify-evenly rounded-md w-full"><span>Engine Oil Change</span>
+                    <FaLongArrowAltRight className="text-2xl mb-2"></FaLongArrowAltRight>
+                </button>
+                <button
+                    className="hover:bg-[#ff3438] mb-2 hover:text-white bg-white text-black px-4 py-2 flex items-center justify-evenly rounded-md w-full"><span>Battery Charge</span>
+                    <FaLongArrowAltRight className="text-2xl mb-2"></FaLongArrowAltRight>
+                </button>
+            </div>
+            <div className="bg-[#000] mt-4 px-8 py-6 text-white rounded-lg">
+                <h2 className="text-2xl font-semibold mb-2">Download</h2>
+                <div className="flex items-center justify-between">
+                    <FaRegFileWord className="text-2xl"></FaRegFileWord>
+                    <div>
+                        <h2>Our Brochure</h2>
+                        <p>Download</p>
+                    </div>
+                    <FaLongArrowAltRight className="text-2xl"></FaLongArrowAltRight>
+                </div>
+                <div className="flex items-center justify-between mt-4">
+                    <FaRegFileWord className="text-2xl"></FaRegFileWord>
+                    <div>
+                        <h2>Company Details</h2>
+                        <p>Download</p>
+                    </div>
+                    <FaLongArrowAltRight className="text-2xl"></FaLongArrowAltRight>
+                </div>
+            </div>
         </div>
     );
 };
