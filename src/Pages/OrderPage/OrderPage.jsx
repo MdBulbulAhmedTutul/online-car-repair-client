89a3f0d253b6bd4ed9c391e58b1ffe5dcfe9b6ed
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-11-22 08:52:04 -0800

    delete a booking services

--- src/Pages/OrderPage/OrderPage.jsx
+++ src/Pages/OrderPage/OrderPage.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../components/provider/AuthProvider";
 import TableRow from "./TableRow";
+import Swal from "sweetalert2";
 
 const OrderPage = () => {
     const { user } = useContext(AuthContext);
@@ -11,6 +12,39 @@ const OrderPage = () => {
             .then(res => res.json())
             .then(data => setBokkingd(data))
     }, [url])
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/delete/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your service has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = bookins.filter(booking => booking._id !== id);
+                            // console.log(remaining);
+                            setBokkingd(remaining);
+                            
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className="max-w-7xl mx-auto">
             <div className="overflow-x-auto my-16">
@@ -19,9 +53,7 @@ const OrderPage = () => {
                     <thead>
                         <tr>
                             <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
+                                <h1>Delete</h1>
                             </th>
                             <th>Name</th>
                             <th>Price</th>
@@ -34,6 +66,7 @@ const OrderPage = () => {
                         {
                             bookins.map(booking =>
                                 <TableRow key={booking._id}
+                                handleDelete={handleDelete}
                                     booking={booking}>
                                 </TableRow>)
                         }
--- src/Pages/OrderPage/TableRow.jsx
+++ src/Pages/OrderPage/TableRow.jsx
@@ -1,17 +1,19 @@
 import { useContext } from "react";
 import { AuthContext } from "../../components/provider/AuthProvider";
+// import Swal from "sweetalert2";
 
 // eslint-disable-next-line react/prop-types
-const TableRow = ({ booking }) => {
-    const {user} = useContext(AuthContext);
+const TableRow = ({ booking, handleDelete }) => {
+    const { user } = useContext(AuthContext);
     // eslint-disable-next-line react/prop-types
-    const { date, email, img, price, title } = booking;
+    const { _id, date, email, img, price, title } = booking;
+    
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td>
                 <div className="flex items-center gap-3">
